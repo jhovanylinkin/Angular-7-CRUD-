b3a50d1171b07e747f1fc3aacef4347c48b785e1
Jhovany Morales Bruno <jhovanymb.info@gmail.com>
2019-01-25 11:58:56 -0700

    error on http protocol catch

--- src/app/questiosapi.service.ts
+++ src/app/questiosapi.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { question } from '../app/models/question';
-import { errorHandler } from '@angular/platform-browser/src/browser';
-
+import { catchError, retry } from 'rxjs/operators';
+import * as M from 'materialize-css';
 @Injectable({
   providedIn: 'root'
 })
@@ -16,11 +16,28 @@ export class QuestiosapiService {
     })
   };
 
+  private handleError(error: HttpErrorResponse) {
+    error.error instanceof ErrorEvent ?
+      M.toast({
+        html: `An error uccurred ${error.error.message}`,
+        classes: 'rounded red'
+      })
+      : M.toast({
+        html: `Backend returned code:  &nbsp; <strong>${error.status}</strong>`,
+        classes: 'rounded orange'
+      });
+      console.log(error.error);
+    return throwError('Something bad happened; please try again later.');
+  }
+
   listQuestions() {
-    return this._Http.get('http://10.0.2.167:8000/polls/');
+    return this._Http.get('http://10.0.2.167:8000/polls/').pipe(
+      catchError(this.handleError)
+    );
   }
 
   addQuestion(_question: question): Observable<question> {
-    return this._Http.post<question>('http://10.0.2.167:8000/polls/results/', _question, this.httpOptions).pipe();
+    return this._Http.post<question>('http://10.0.2.167:8000/polls/results/', _question, this.httpOptions).pipe(
+      catchError(this.handleError));
   }
 }
